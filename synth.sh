f890eb3828496c9ae3f8e5c5014ba83f445d818c
Barry O'reilly <gundaetiapo@gmail.com>
2017-11-28 18:31:05 -0800

    Tweak fluidsynth invocation

--- synth.sh
+++ synth.sh
@@ -1,3 +1,3 @@
 # Timbres of Heaven http://midkar.com/soundfonts/
-fluidsynth -a alsa -m alsa_seq -g0.5 -s -i ~/soundfonts/Timbres\ Of\ Heaven\ GM_GS_XG_SFX\ V\ 3.4\ Final.sf2
+fluidsynth -a alsa -m alsa_seq -g0.4 -s -i ~/soundfonts/Timbres\ Of\ Heaven\ GM_GS_XG_SFX\ V\ 3.4\ Final.sf2
 #timidity -iA -A100 -Os --quiet
